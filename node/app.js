45e1b7d785208238d8ebd341ba1fbda26803dbc4
yjiahao123 <yjiahao123@gmail.com>
2024-08-25 23:21:16 +0800

    added simple prompts

--- node/app.js
+++ node/app.js
@@ -1,3 +1,29 @@
-const hello = "Hello";
+import { ChatGroq } from "@langchain/groq";
 
-console.log(hello);
+const llm = new ChatGroq({
+    model: "llama-3.1-8b-instant",
+    temperature: 0,
+  });
+
+const prompt = `
+You are a professional investor that follows the Warren Buffett philosophy of stock-picking.
+The human is someone who is trying to pick up stock-investing.
+
+Assist the human by providing detailed feedback about his investing ideas and help him with his stock analysis.
+`
+
+const aiMsg = await llm.invoke(
+    [
+        [
+            "system",
+            prompt,
+        ],
+        [
+            "human",
+            "How can I perform fundamental analysis?"
+        ],
+    ]
+);
+
+// print out content of message
+console.log(aiMsg.content);
