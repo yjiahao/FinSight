625715f13074c5a78d46eca18dd1cc8dee14c99a
yjiahao123 <yjiahao123@gmail.com>
2024-08-26 10:13:55 +0800

    added comments to the llm

--- node/app.js
+++ node/app.js
@@ -1,26 +1,33 @@
 import { ChatGroq } from "@langchain/groq";
 
-const llm = new ChatGroq({
-    model: "llama-3.1-8b-instant",
-    temperature: 0,
-  });
+// define the model
+const llm = new ChatGroq(
+    {
+        model: "llama-3.1-8b-instant",
+        temperature: 0.5,
+    }
+);
 
-const prompt = `
+// create prompt template
+const systemPrompt = `
 You are a professional investor that follows the Warren Buffett philosophy of stock-picking.
 The human is someone who is trying to pick up stock-investing.
 
 Assist the human by providing detailed feedback about his investing ideas and help him with his stock analysis.
 `
 
+var humanPrompt = "How can I perform fundamental analysis?";
+
+// call the prompt on the llm
 const aiMsg = await llm.invoke(
     [
         [
             "system",
-            prompt,
+            systemPrompt,
         ],
         [
             "human",
-            "How can I perform fundamental analysis?"
+            humanPrompt
         ],
     ]
 );
