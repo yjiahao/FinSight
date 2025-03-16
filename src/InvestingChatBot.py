4cfcf652ea250791c73e91ef106158f911063da1
yjiahao123 <yjiahao123@gmail.com>
2025-03-16 17:25:35 +0800

    update chatbot into agent with chat history

--- src/InvestingChatBot.py
+++ src/InvestingChatBot.py
@@ -4,6 +4,7 @@ from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.chat_message_histories import RedisChatMessageHistory
+from langchain.agents import AgentExecutor, create_tool_calling_agent
 
 from dotenv import load_dotenv
 
@@ -27,15 +28,17 @@ class InvestingChatBot:
                     If you do not know the answer, just return "I don't know".
                     """
 
-    def __init__(self, model: str, temperature: float, session_id: str):
+    def __init__(self, model: str, temperature: float, tools: list[callable], session_id: str):
 
         '''Initialize the model, temperature, and the session id'''
 
         self.model = model
         self.temperature = temperature
         self.session_id = session_id
+        # Tools for the agent to use
+        self.tools = tools
 
-        self.chat = self.create_llm_chain_with_history()
+        self.agent = self.create_agent_with_history()
         
 
     def get_message_history(self) -> RedisChatMessageHistory:
@@ -51,9 +54,9 @@ class InvestingChatBot:
         history = self.get_message_history()
         history.clear()
     
-    def create_llm_chain_with_history(self) -> RunnableWithMessageHistory:
+    def create_agent_with_history(self) -> RunnableWithMessageHistory:
         '''
-        Create a runnable with message history. Message history saved to redis database for persistence
+        Create an agent with message history. Message history saved to redis database for persistence
         '''
 
         chat = ChatGroq(
@@ -65,32 +68,35 @@ class InvestingChatBot:
         prompt = ChatPromptTemplate.from_messages(
             [
                 ("system", self.system_prompt),
-                MessagesPlaceholder(variable_name="history"), # add in chat history
-                ("human", "{input}")
+                # First put the history
+                ("placeholder", "{chat_history}"),
+                # Then the new input
+                ("human", "{input}"),
+                # Finally the scratchpad
+                ("placeholder", "{agent_scratchpad}"),
             ]
         )
         
-        # create the chain
-        chain = prompt | chat
+        agent = create_tool_calling_agent(chat, self.tools, prompt)
+        agent_executor = AgentExecutor(agent=agent, tools=self.tools)
 
-        # add message history to the llm
-        chain_with_message_history = RunnableWithMessageHistory(
-            chain,
-            self.get_message_history, # add in method to get message history
+        agent_with_chat_history = RunnableWithMessageHistory(
+            agent_executor,
+            self.get_message_history,
             input_messages_key="input",
-            history_messages_key="history",
+            history_messages_key="chat_history",
         )
 
-        return chain_with_message_history
+        return agent_with_chat_history
     
-    def prompt(self, ability: str, input: str) -> str:
+    # UNTESTED METHOD
+    def prompt(self, input: str) -> str:
         '''
-        Prompt the LLM, get a response in string
+        Prompt the agent, get a response in string
         '''
-        
-        response = self.chat.invoke(
-            {"ability": ability, "input": input},
+        response = self.agent.invoke(
+            {"input": input},
             config={"configurable": {"session_id": self.session_id}},
         )
 
-        return response.content
+        return response['output']
