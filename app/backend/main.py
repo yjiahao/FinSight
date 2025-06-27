9cd4f20f9855c258ac311d6369d26967dca8b5ef
yjiahao123 <yjiahao123@gmail.com>
2025-06-27 23:05:28 +0800

    add CORSMiddleware to allow requests from origins other than backend

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.backend.api.endpoints.v1 import router
 from app.backend.core.config import settings
 
+# declare origins for CORS
+origins = [
+    "https://localhost:8000",
+    "https://localhost:5173"
+]
+
 app = FastAPI(title=settings.app_name)
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
