06c9929d17892982f040dca3857ed5b5dd389b3f
yjiahao123 <yjiahao123@gmail.com>
2025-07-06 18:15:55 +0800

    add todo task for future

--- app/backend/main.py
+++ app/backend/main.py
@@ -12,8 +12,10 @@ from app.backend.services.session_manager import SessionManager
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # load state variables
+
     # create a global instance of the session manager
     app.state.session_manager = SessionManager()
+    
     # TODO: currently we are creating a new ChatHistory for each session, meaning the embedding model will be loaded multiple times.
     #       we should consider using a shared instance of the embedding model and chat history to save resources.
     #       this will require some refactoring of the ChatHistory class to allow for shared instances
