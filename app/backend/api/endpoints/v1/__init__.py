10bfcc42d699f3e9a151faedf1a51a64d3a0fd58
yjiahao123 <yjiahao123@gmail.com>
2025-07-06 17:19:45 +0800

    add endpoints for authentication, update chat import

--- app/backend/api/endpoints/v1/__init__.py
+++ app/backend/api/endpoints/v1/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
-from . import home, chat
+from . import home, chat, auth
 
 router = APIRouter()
+
 router.include_router(home.router)
 router.include_router(chat.router)
+router.include_router(auth.router)
